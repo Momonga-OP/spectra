e3334cb88a3914f9ec9253ee0ee19af9f093bc2b
Momonga-OP <latifdofus@gmail.com>
2025-06-27 04:47:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ async def on_error(event: str, *args, **kwargs):
 EXTENSIONS = [
     'cogs.general', 'cogs.moderation', 'cogs.poll', 'cogs.admin', 'cogs.gtoguild', 'cogs.save', 'cogs.key', 'cogs.link', 'cogs.log',
     'cogs.relocate', 'cogs.watermark', 'cogs.serverstats', 'cogs.talk', 'cogs.write', 'cogs.alerts', 'cogs.makeup', 'cogs.inviteafl',
-    'cogs.watermark_user', 'cogs.attack', 'cogs.role', 'cogs.metiers', 'cogs.percopos', 'cogs.congrats', 'cogs.members', 'cogs.tag',
+    'cogs.watermark_user', 'cogs.attack', 'cogs.role', 'cogs.metiers', 'cogs.percopos', 'cogs.congrats', 'cogs.members', 'cogs.rulesafl',
     'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.percoattack', 'cogs.sure', 'cogs.music', 'cogs.clone',
     'cogs.rbg', 'cogs.bow', 'cogs.welcomesparta', 'cogs.contract', 'cogs.profession', 'cogs.afl', 'cogs.voicechannel', 'cogs.kamas',
     'cogs.super', 'cogs.translator', 'cogs.spotify', 'cogs.voice', 'cogs.youtubemp4', 'cogs.ecologia', 'cogs.invite', 'cogs.translation_voice',
