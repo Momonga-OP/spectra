61ffba8f43ea405c1c1751bbc4b643dda42cac8a
Momonga-OP <latifdofus@gmail.com>
2025-07-01 05:18:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ EXTENSIONS = [
     'cogs.general', 'cogs.moderation', 'cogs.poll', 'cogs.admin', 'cogs.gtoguild', 'cogs.save', 'cogs.key', 'cogs.link', 'cogs.log', 'cogs.welcomeafl',
     'cogs.relocate', 'cogs.watermark', 'cogs.serverstats', 'cogs.talk', 'cogs.write', 'cogs.alerts', 'cogs.makeup', 'cogs.pdf',
     'cogs.watermark_user', 'cogs.attack', 'cogs.role', 'cogs.metiers', 'cogs.percopos', 'cogs.congrats', 'cogs.members', 'cogs.rulesafl',
-    'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.percoattack', 'cogs.sure', 'cogs.music', 'cogs.clone',
+    'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.percoattack', 'cogs.sure', 'cogs.music', 'cogs.clone', 'cogs.tag',
     'cogs.rbg', 'cogs.bow', 'cogs.welcomesparta', 'cogs.contract', 'cogs.profession', 'cogs.afl', 'cogs.voicechannel', 'cogs.kamas',
     'cogs.super', 'cogs.translator', 'cogs.spotify', 'cogs.voice', 'cogs.youtubemp4', 'cogs.ecologia', 'cogs.invite', 'cogs.translation_voice',
 ]
