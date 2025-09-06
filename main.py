db5191809d31749b752c5d89d4ea0d07df281ea8
Momonga-OP <latifdofus@gmail.com>
2025-09-06 10:04:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ EXTENSIONS = [
     'cogs.relocate', 'cogs.watermark', 'cogs.serverstats', 'cogs.talk', 'cogs.write', 'cogs.alerts', 'cogs.makeup', 'cogs.pdf', 'cogs.teamspvp', 'cogs.lottery',
     'cogs.watermark_user', 'cogs.attack', 'cogs.role', 'cogs.metiers', 'cogs.percopos', 'cogs.congrats', 'cogs.members', 'cogs.rulesafl', 'cogs.exportroles',
     'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.prism', 'cogs.sure', 'cogs.music', 'cogs.clone', 'cogs.tag', 'cogs.time', 'cogs.serverafl',
-    'cogs.rbg', 'cogs.bow', 'cogs.welcomesparta', 'cogs.contract', 'cogs.profession', 'cogs.afl', 'cogs.voicechannel', 'cogs.pin', 'cogs.memberstats',
+    'cogs.rbg', 'cogs.bow', 'cogs.welcomesparta', 'cogs.contract', 'cogs.profession', 'cogs.afl', 'cogs.voicechannel', 'cogs.pin', 'cogs.memberstats', 'cogs.announcement',
     'cogs.super', 'cogs.translator', 'cogs.spotify', 'cogs.voice', 'cogs.youtubemp4', 'cogs.ecologia', 'cogs.invite', 'cogs.translation_voice',
 ]
 
@@ -172,6 +172,7 @@ if __name__ == "__main__":
     except Exception as e:
         logger.exception("Bot encountered an error and stopped")
         remove_lock()  # Clean up on error
+
 
 
 
